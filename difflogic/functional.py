a3ec4b6cac67a669cf87b051bc770fa4092e5542
dimzachiotis <dimzachiotis@gmail.com>
2026-01-01 17:50:22 +0200

    added comments on functional.py

--- difflogic/functional.py
+++ difflogic/functional.py
@@ -22,12 +22,19 @@ BITS_TO_NP_DTYPE = {8: np.int8, 16: np.int16, 32: np.int32, 64: np.int64}
 # | 13 | A implies B          | 1     | 1     | 0     | 1     |
 # | 14 | not(A and B)         | 1     | 1     | 1     | 0     |
 # | 15 | 1                    | 1     | 1     | 1     | 1     |
+"""
+A implies B = Not(A) or B
+B implies A = Not(B) or A
+"""
 
+"""This function returns the value of the form of the ith logic gate shown above """
 def bin_op(a, b, i):
     assert a[0].shape == b[0].shape, (a[0].shape, b[0].shape)
+    """ if a and b have incompatible number of rows then stop and print their number of their rows"""
     if a.shape[0] > 1:
         assert a[1].shape == b[1].shape, (a[1].shape, b[1].shape)
-
+    """ if a and b have incompatible number of collumns then stop and print their number of their collumns"""
+    """ Makes sure a and b have the same size """
     if i == 0:
         return torch.zeros_like(a)
     elif i == 1:
@@ -60,6 +67,7 @@ def bin_op(a, b, i):
         return 1 - a * b
     elif i == 15:
         return torch.ones_like(a)
+"""Implements the relaxation form of each logic gate"""
 
 
 def bin_op_s(a, b, i_s):
@@ -68,25 +76,31 @@ def bin_op_s(a, b, i_s):
         u = bin_op(a, b, i)
         r = r + i_s[..., i] * u
     return r
+"""Apply all logic operations to a and b, weight each result based on i_s[..., i], and sum them """
 
 
 ########################################################################################################################
 
-
+"""constructs out_dim random index pairs from x """
 def get_unique_connections(in_dim, out_dim, device='cuda'):
     assert out_dim * 2 >= in_dim, 'The number of neurons ({}) must not be smaller than half of the number of inputs ' \
                                   '({}) because otherwise not all inputs could be used or considered.'.format(
         out_dim, in_dim
     )
+    """In case you want cpu, you have to initialize it device='cpu'"""
 
     x = torch.arange(in_dim).long().unsqueeze(0)
+    """x is a 2D shape (1,in_dim)"""
 
     # Take pairs (0, 1), (2, 3), (4, 5), ...
     a, b = x[..., ::2], x[..., 1::2]
+    """a takes elements of x even indices, b of odd indices"""
+
     if a.shape[-1] != b.shape[-1]:
         m = min(a.shape[-1], b.shape[-1])
         a = a[..., :m]
         b = b[..., :m]
+    """if a and b have different lenghts, thim them to the same lenght"""
 
     # If this was not enough, take pairs (1, 2), (3, 4), (5, 6), ...
     if a.shape[-1] < out_dim:
@@ -106,18 +120,24 @@ def get_unique_connections(in_dim, out_dim, device='cuda'):
         b = torch.cat([b, b_], dim=-1)
         offset += 1
         assert a.shape[-1] == b.shape[-1], (a.shape[-1], b.shape[-1])
+    """generate shifted pairs, until a.shape[-1] >= out_dim
+        (0,2), (1,3), (2,4), ...
+        (0,3), (1,4), ...
+        ...
+    """
 
     if a.shape[-1] >= out_dim:
         a = a[..., :out_dim]
         b = b[..., :out_dim]
     else:
         assert False, (a.shape[-1], offset, out_dim)
+    """keep out_dim pairs, else fail if we didnt get enough pairs"""
 
     perm = torch.randperm(out_dim)
-
+    """ Generates a random permutation of indices """
     a = a[:, perm].squeeze(0)
     b = b[:, perm].squeeze(0)
-
+    """shuffle pairs"""
     a, b = a.to(torch.int64), b.to(torch.int64)
     a, b = a.to(device), b.to(device)
     a, b = a.contiguous(), b.contiguous()
@@ -126,7 +146,7 @@ def get_unique_connections(in_dim, out_dim, device='cuda'):
 
 ########################################################################################################################
 
-
+"""GradFactor is a class with functions that pass the input x forward unchanged (forward) but scale the gradient during backpropagation by a factor f(bachward)"""
 class GradFactor(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x, f):
