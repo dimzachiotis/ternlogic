8280958c73a41bb5476a562c741d8b5eb25de24f
dimzachiotis <dimzachiotis@gmail.com>
2026-01-10 10:14:02 +0200

    changed difflogic.py

--- difflogic/difflogic.py
+++ difflogic/difflogic.py
@@ -1,5 +1,5 @@
 import torch
-import difflogic_cuda
+#import difflogic_cuda
 import numpy as np
 from .functional import bin_op_s, get_unique_connections, GradFactor
 from .packbitstensor import PackBitsTensor
@@ -16,7 +16,7 @@ class LogicLayer(torch.nn.Module):
             self,
             in_dim: int,
             out_dim: int,
-            device: str = 'cuda',
+            device: str = 'cpu',
             grad_factor: float = 1.,
             implementation: str = None,
             connections: str = 'random',
@@ -107,9 +107,9 @@ class LogicLayer(torch.nn.Module):
         #Shape check 
 
         if self.indices[0].dtype == torch.int64 or self.indices[1].dtype == torch.int64:
-            print(self.indices[0].dtype, self.indices[1].dtype)
+            #print(self.indices[0].dtype, self.indices[1].dtype)
             self.indices = self.indices[0].long(), self.indices[1].long()
-            print(self.indices[0].dtype, self.indices[1].dtype)
+            #print(self.indices[0].dtype, self.indices[1].dtype)
         #Ensures right tensor indexing 
 
         a, b = x[..., self.indices[0]], x[..., self.indices[1]]
