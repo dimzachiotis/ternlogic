f4653ba3963029d550bed275f517da137b09bb77
dimzachiotis <dimzachiotis@gmail.com>
2026-01-13 11:48:53 +0200

    changed threshold when removing borders

--- experiments/mnist_dataset.py
+++ experiments/mnist_dataset.py
@@ -22,8 +22,9 @@ from torchvision.datasets.vision import VisionDataset
 class MNISTRemoveBorderTransform:
     # __call__ expects an image with values in [0, 1]. ToTensor() has to be applied previously,cause if not it will brake
     def __call__(self, image: torch.Tensor) -> torch.Tensor:
-        horizontal_black_lines = (image == 0.).all(dim=2)
+        horizontal_black_lines = (image <= 0.25).all(dim=2)
         #detects entire horizontal black rows. returns tensor with boolean values
+        #set the threshold to 0.25
         top_black = 0
         while horizontal_black_lines[0, top_black] and top_black < 14:
             top_black += 1
@@ -48,7 +49,8 @@ class MNISTRemoveBorderTransform:
         assert top_black + bottom_black == 8, (top_black, bottom_black)
         image = image[:, top_black:28 - bottom_black]
         #Crop vertically
-        vertical_black_lines = (image == 0.).all(dim=1)
+        vertical_black_lines = (image <= 0.25).all(dim=1)
+        #set the threshold to 0.25
         #Detects entire black columns.
         left_black = 0
         #same idea as rows
