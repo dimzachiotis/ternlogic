a81838be788edb8064eac30534642f79cc1728e0
dimzachiotis <dimzachiotis@gmail.com>
2026-03-02 12:20:31 +0200

    necessary definition changed

--- difflogic/__init__.py
+++ difflogic/__init__.py
@@ -1,4 +1,5 @@
 from .difflogic import LogicLayer, GroupSum
 from .packbitstensor import PackBitsTensor
 from .compiled_model import CompiledLogicNet
-from .compiled_ternary_model_python import CompiledPython
+from .compiled_ternary_model_python import CompiledTernaryPython
+from .compiled_binary_model_python import CompiledBinaryPython
