33d7ab09a026ad59e37e5aabd8885faaffa0b957
dimzachiotis <dimzachiotis@gmail.com>
2026-03-02 11:47:12 +0200

    added pyhton compilation in main

--- experiments/main.py
+++ experiments/main.py
@@ -11,6 +11,7 @@ import numpy as np
 import torch
 import torchvision
 from tqdm import tqdm
+import json
 
 from results_json import ResultsJSON
 #Store experiment results in JSON files
@@ -28,6 +29,7 @@ if top_level_dir not in sys.path:
 from difflogic.difflogic import LogicLayer, GroupSum
 from difflogic.packbitstensor import PackBitsTensor
 from difflogic.compiled_model import CompiledLogicNet
+from difflogic.compiled_model_python import CompiledPython
 
 device ='cpu' if  not torch.cuda.is_available() else 'cuda'
 #if no cuda available, then use cpu
@@ -423,71 +425,56 @@ if __name__ == '__main__':
         print(f"Neuron weights saved as JSON: {json_filename}")
 
     ####################################################################################################################
-    #Model Compilation (Optional)
-
+    
+    #Model Python Compilation (Optional)
     if args.compile_model:
         print('\n' + '='*80)
-        print(' Converting the model to C code and compiling it...')
+        print(' Compiling model with Python...')
         print('='*80)
-
-        for opt_level in range(4):
-
-            for num_bits in [
-                # 8,
+        for num_bits in [
                 # 16,
                 # 32,
                 64
             ]:
-                os.makedirs('lib', exist_ok=True)
-                save_lib_path = 'lib/{:08d}_{}.so'.format(
-                    args.experiment_id if args.experiment_id is not None else 0, num_bits
-                )
-                #Converts logic network into pure C code
-                compiled_model = CompiledLogicNet(
-                    model=model,
-                    num_bits=num_bits,
-                    cpu_compiler='gcc',
-                    # cpu_compiler='clang',
-                    verbose=True,
-                )
-
-                compiled_model.compile(
-                    opt_level=1 if args.num_layers * args.num_neurons < 50_000 else 0,
-                    save_lib_path=save_lib_path,
-                    verbose=True
+                #Creates a CompiledPython object
+                compiled_model = CompiledPython(
+                model=model,
+                verbose=False,
+                num_bits=num_bits
                 )
 
                 correct, total = 0, 0
                 with torch.no_grad():
                     for (data, labels) in torch.utils.data.DataLoader(test_loader.dataset, batch_size=int(1e6), shuffle=False):
-                        data = torch.nn.Flatten()(data).bool().numpy()
-                        #Executes compiled C model
-                        output = compiled_model(data, verbose=True)
+                        #flattens the input tensor to 1D per sample and converts the data to boolean values (0 or 1). shape[batch size,product of dimesions of data]
+                        data = torch.nn.Flatten()(data).bool()
+                        #Returns predictions as outputs shape[batch size,number of classes]
+                        output = compiled_model.forward(data)
 
                         correct += (output.argmax(-1) == labels).float().sum()
                         total += output.shape[0]
                 #Accuracy of compiled model
-                acc3 = correct / total
-                print('COMPILED MODEL', num_bits, acc3)
+                python_acc = correct / total
+                print('COMPILED PYTHON MODEL', num_bits , python_acc)
 
-    #Store Accuracy of c compilation
-    if args.experiment_id is not None:
-        # Ensure results folder exists
-        os.makedirs('./results', exist_ok=True)
+                #Store Accuracy of python compilation
+                if args.experiment_id is not None:
+                    # Ensure results folder exists
+                    os.makedirs('./results', exist_ok=True)
 
-        # Prepare filename
-        json_filename = f"./results/{args.experiment_id}_c.json"
+                    # Prepare filename
+                    json_filename = f"./results/{args.experiment_id}_{num_bits}_python.json"
 
-        # If acc3 is a tensor, convert it to float
-        if isinstance(acc3, torch.Tensor):
-            acc3 = acc3.item()  # gets the scalar value as a float
+                    # If python_acc is a tensor, convert it to float
+                    if isinstance(python_acc, torch.Tensor):
+                        python_acc = python_acc.item()  # gets the scalar value as a float
 
-        # Wrap in dict for JSON
-        acc_data = {'accuracy': acc3}
+                    # Wrap in dict for JSON
+                    acc_data = {'accuracy': python_acc}
 
+                    # Save to JSON
+                    with open(json_filename, "w") as f:
+                        json.dump(acc_data, f, indent=4)
 
-        # Save to JSON
-        with open(json_filename, "w") as f:
-            json.dump(acc_data, f, indent=4)
-
-        print(f"Accuracy saved as JSON: {json_filename}")        
+                    print(f"python_acc saved as JSON: {json_filename}") 
+            
