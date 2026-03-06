0de0d5043274ed6fac2e43175792f1f0ea81f3be
dimzachiotis <dimzachiotis@gmail.com>
2026-03-06 13:05:02 +0200

    changed file name during storing

--- experiments/results_json.py
+++ experiments/results_json.py
@@ -68,14 +68,14 @@ class ResultsJSON(object):
         #Converts everything into a JSON string
 
 
-        with open(os.path.join(self.path, f'{self.eid}_total_tern_1.json'), mode='w') as f:
+        with open(os.path.join(self.path, f'{self.eid}_total_tern_2.json'), mode='w') as f:
             f.write(json_str)
         #Writes JSON string to file
     
     #Loads experiment results from a file
     @staticmethod
     def load(eid: int, path: str, get_dict=False):
-        with open(os.path.join(path, f'{eid}_total_tern_1.json'), mode='r') as f:
+        with open(os.path.join(path, f'{eid}_total_tern_2.json'), mode='r') as f:
             data = json.loads(f.read())
         #Converts JSON back into a Python dictionary
 
