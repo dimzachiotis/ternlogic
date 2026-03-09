721d16fd051c20b61d4368ce0d2a085c005e5072
dimzachiotis <dimzachiotis@gmail.com>
2026-03-09 11:17:35 +0200

    save also loss value to check convergence

--- experiments/main.py
+++ experiments/main.py
@@ -393,6 +393,7 @@ if __name__ == '__main__':
             train_accuracy_eval_mode = eval(model, train_loader, mode=False)
             test_accuracy_eval_mode = eval(model, test_loader, mode=False)
             test_accuracy_train_mode = eval(model, test_loader, mode=True)
+            loss_value = loss
 
             r = {
                 'train_acc_eval_mode': train_accuracy_eval_mode,
@@ -401,6 +402,7 @@ if __name__ == '__main__':
                 'valid_acc_train_mode': valid_accuracy_train_mode,
                 'test_acc_eval_mode': test_accuracy_eval_mode,
                 'test_acc_train_mode': test_accuracy_train_mode,
+                'loss': loss_value,
             }
 
             if args.packbits_eval:
