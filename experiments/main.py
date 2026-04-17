b472d8d07cfbbe6cac24d3e120559487b6800164
Dimitrios Zachiotis <dimzachiotis@gmail.com>
2026-04-17 09:58:04 +0300

    main

--- experiments/main.py
+++ experiments/main.py
@@ -380,7 +380,7 @@ if __name__ == '__main__':
         mlflow.set_experiment(exp_name)    
 
     #Gives your run a readable name.
-    mlflow.start_run(run_name=f"{args.dataset}_k{args.num_neurons}_l{args.num_layers}_seed{args.seed}_lr{args.learning_rate}_tern_cpu")
+    mlflow.start_run(run_name=f"{args.dataset}_k{args.num_neurons}_l{args.num_layers}_seed{args.seed}_lr{args.learning_rate}_tern")
     
     #creates arg object
     ####################################################################################################################
