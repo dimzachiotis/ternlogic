c68825b6be24e031dc706d79785fe44d7f5c2f17
Dimitrios Zachiotis <dimzachiotis@gmail.com>
2026-04-22 13:10:03 +0300

    ok

--- experiments/main.py
+++ experiments/main.py
@@ -395,7 +395,9 @@ if __name__ == '__main__':
                 with torch.no_grad():
                     for (data, labels) in torch.utils.data.DataLoader(test_loader.dataset, batch_size=int(1e6), shuffle=False):
                         #flattens the input tensor to 1D per sample and converts the data to boolean values (0 or 1). shape[batch size,product of dimesions of data]
-                        data = torch.nn.Flatten()(data).bool()
+                        data = torch.nn.Flatten()(data)
+                        data = data.to(device)
+                        labels=labels.to(device)
                         #Returns predictions as outputs shape[batch size,number of classes]
                         output = compiled_model.forward(data)
 
